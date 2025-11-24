d92eb7fa98228c3cf401c264bcfb034e17c50f9a
Marcos Aurelio <marcos.aucorrea@gmail.com>
2025-11-24 17:37:40 -0300

    Inicio projeto

---
+++ SmartLanche/SmartLanche/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SmartLanche
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
