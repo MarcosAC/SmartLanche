604042efeecb4d58fa9e0e74faaa6028d3d3c3f0
Marcos Aurelio <marcos.aucorrea@gmail.com>
2025-11-27 19:49:52 -0300

    Inicio MailViewModel

---
+++ SmartLanche/SmartLanche/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartLanche.ViewModels
+{
+    public partial class MainViewModel : BaseViewModel
+    {
+        private readonly ConfigurationService _configuration;
+        public ProductRegistrationViewModel? ProductRegistrationViewModel { get; }
+
+        public string AppName => _configuration.AppName;
+        public string LogoPath => _configuration.LogoPath;
+
+        public MainViewModel(ConfigurationService configuration, ProductRegistrationViewModel product)
+        {
+            _configuration = configuration;
+            ProductRegistrationViewModel = product;
+        }
+    }
+}
