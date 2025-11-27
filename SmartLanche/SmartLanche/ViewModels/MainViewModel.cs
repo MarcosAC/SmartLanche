73fec3d0d4598b86f01457929ff5ef34b0deee2c
Marcos Aurelio <marcos.aucorrea@gmail.com>
2025-11-27 20:00:19 -0300

    Inicio MainViewModel

--- SmartLanche/SmartLanche/ViewModels/MainViewModel.cs
+++ SmartLanche/SmartLanche/ViewModels/MainViewModel.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SmartLanche.Services;
 
 namespace SmartLanche.ViewModels
 {
