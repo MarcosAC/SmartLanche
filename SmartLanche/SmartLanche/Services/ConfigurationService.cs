64a48f0010c01a94ffa990485f9a0ca31dc2ea8e
Marcos Aurelio <marcos.aucorrea@gmail.com>
2025-11-27 19:57:03 -0300

    Servico de configuracoes

---
+++ SmartLanche/SmartLanche/Services/ConfigurationService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SmartLanche.Services
+{
+    public class ConfigurationService
+    {
+        private readonly IConfiguration _configuration;
+        public ConfigurationService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string AppName => _configuration.GetSection("SmartLanche")["Name"] ?? "SmartLanche";        
+        public string LogoPath => _configuration.GetSection("SmartLanche")["Logo"] ?? "SmartLanche";
+        public string Theme => _configuration.GetSection("SmartLanche")["Theme"] ?? "SmartLanche";
+    }
+}
