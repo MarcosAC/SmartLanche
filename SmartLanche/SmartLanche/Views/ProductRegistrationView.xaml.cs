f2dbd08231624f2618e5a01d3d9d293f7f22d807
Marcos Aurelio <marcos.aucorrea@gmail.com>
2025-11-30 20:52:39 -0300

    Remocao de using desnecessarios

--- SmartLanche/SmartLanche/Views/ProductRegistrationView.xaml.cs
+++ SmartLanche/SmartLanche/Views/ProductRegistrationView.xaml.cs
@@ -1,18 +1,8 @@
-﻿using SmartLanche.Models;
+﻿using Microsoft.Extensions.DependencyInjection;
+using SmartLanche.Models;
 using SmartLanche.Services;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SmartLanche.Views
 {    
