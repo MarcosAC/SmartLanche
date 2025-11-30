8e7805fc86049ab26aadd92483ab97c083294736
Marcos Aurelio <marcos.aucorrea@gmail.com>
2025-11-30 20:50:40 -0300

    Criacao DbContext

---
+++ SmartLanche/SmartLanche/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartLanche.Models;
+
+namespace SmartLanche.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products => Set<Product>();
+        //public DbSet<Client> Clients => Set<Client>();
+        //public DbSet<Order> Orders => Set<Order>();
+        //public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+        //public DbSet<StockItem> StockItems => Set<StockItem>();
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    modelBuilder.Entity<Order>().Property(order => order.Status).HasConversion<string>();
+        //}
+
+    }
+}
