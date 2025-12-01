1a006844255c4a87819fa6fc0fb4c80b04a0fb3d
Marcos Aurelio <marcos.aucorrea@gmail.com>
2025-12-01 17:51:38 -0300

    Criacao tela principal

---
+++ SmartLanche/SmartLanche/Views/MainWindowView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace SmartLanche.Views
+{
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView(ViewModels.MainWindowViewModel vm)
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+    }
+}
