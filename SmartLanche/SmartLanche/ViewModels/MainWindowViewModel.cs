2f7ad7797dbbc551c3efdeecd7702819d3f02bc6
Marcos Aurelio <marcos.aucorrea@gmail.com>
2025-12-01 17:50:05 -0300

    Troca de nome da viewmodel para corrigir erro de ambiguidade

---
+++ SmartLanche/SmartLanche/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using SmartLanche.Services;
+
+namespace SmartLanche.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public ProductRegistrationViewModel? ProductRegistrationViewModel { get; }
+
+        public string AppName {  get; }
+        public string LogoPath { get; }
+
+        public MainWindowViewModel(IConfigurationService configuration, ProductRegistrationViewModel product)
+        {
+            AppName = configuration.GetNameApp();
+            LogoPath = configuration.GetLogoPath();
+
+            ProductRegistrationViewModel = product;
+        }
+    }
+}
