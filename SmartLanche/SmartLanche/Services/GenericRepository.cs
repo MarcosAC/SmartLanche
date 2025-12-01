f097b95055d0a02fddec2f2f535e3e555d727f75
Marcos Aurelio <marcos.aucorrea@gmail.com>
2025-11-30 21:27:00 -0300

    Implementacao repositorio generico

---
+++ SmartLanche/SmartLanche/Services/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartLanche.Data;
+
+namespace SmartLanche.Services
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context) 
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public Task AddAsync(T entity)
+        {
+            _dbSet.Add(entity);
+            return _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+
+            if (entity == null) return;
+
+            _dbSet.Remove(entity);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+
+            return _context.SaveChangesAsync();
+        }
+    }
+}
