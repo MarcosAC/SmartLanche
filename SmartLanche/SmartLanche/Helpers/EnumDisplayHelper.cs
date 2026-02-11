4cc2597b965a75623838e258fc1e3392e956b03c
Marcos Aurelio <marcos.aucorrea@gmail.com>
2026-02-11 18:22:53 -0300

    Filtro de pesquisa da tela historico de estoque

--- SmartLanche/SmartLanche/Helpers/EnumDisplayHelper.cs
+++ SmartLanche/SmartLanche/Helpers/EnumDisplayHelper.cs
@@ -6,12 +6,12 @@ namespace SmartLanche.Helpers
     public static class EnumDisplayHelper
     {
         public static string GetDisplayName(this Enum enumValue)
-        {            
+        {
             var fieldInfo = enumValue.GetType().GetField(enumValue.ToString());
-            
+
             var displayAttribute = fieldInfo?.GetCustomAttribute<DisplayAttribute>(false);
-            
+
             return displayAttribute?.Name ?? enumValue.ToString();
-        }
+        }        
     }
 }
---
+++ SmartLanche/SmartLanche/Helpers/EnumToDisplayNameConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace SmartLanche.Helpers
+{
+    public class EnumToDisplayNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Enum enumValue)
+            {                
+                return enumValue.GetDisplayName();
+            }
+
+            return value?.ToString() ?? string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
